63e6cd7f1aba53324c82ce215d4c4465a1c00838
Peter Mitrano <pmitrano@umich.edu>
2020-12-19 16:52:25 -0500

    Update create_ws.sh

--- install_scripts/create_ws.sh
+++ install_scripts/create_ws.sh
@@ -2,10 +2,9 @@
 
 WS_PATH=~/catkin_ws
 
-source /opt/ros/melodic/setup.bash
 mkdir -p $WS_PATH/src
 cd $WS_PATH
+catkin_init_workspace
+cd ..
 catkin build
 source devel/setup.bash
-catkin_init_workspace
-
